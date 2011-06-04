a9ded14c7b43286e7162eb4803952a918c8b9a5e
Jonathan Dance <jd@wuputah.com>
2011-06-04 01:53:56 -0400

    initial commit w/ Gemfile and Procfile

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'pg'
+gem 'redis'
+gem 'redis-store'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisExample::Application.config.secret_token = '915afd1a9be86bf32e645c6ba68d9e2b764ed5d8b2d3fe77a5150fa17ee14aa1e5de84538bb61cfdfa62aced130d7536c3340af284fdfc375cf3fb48c1e42393'
