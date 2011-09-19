92c236d783757db4d83bb9f04dc6b485aac3d719
Luis Reis <luis.m.reis@gmail.com>
2011-09-20 00:40:04 +0200

    Remove extra = on session_store definition.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-RedisExample::Application.config.session_store = :redis_session_store, { :servers => ENV['REDISTOGO_URL'] || 'redis://127.0.0.1', :expires => 86400 }
+RedisExample::Application.config.session_store :redis_session_store, { :servers => ENV['REDISTOGO_URL'] || 'redis://127.0.0.1', :expires => 86400 }
